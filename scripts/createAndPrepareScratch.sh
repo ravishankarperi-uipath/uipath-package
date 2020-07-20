bc4b6c43ea44b6e2abfd7591c795b6a586402824
uipathdevops <63786696+uipathdevops@users.noreply.github.com>
2020-07-20 14:47:23 +0100

    package validation CI enhancements (#30)
    * add filters
    
    * package validation
    
    * fix yaml issues
    
    * fix syntax issue on pipeline
    
    * minor change
    
    * remove pipe
    
    * removee pipe
    
    * compile only package classes
    
    * corrected compile param
    
    Co-authored-by: Girish Lakshmanan <girish.la@gmail.com>

--- scripts/createAndPrepareScratch.sh
+++ scripts/createAndPrepareScratch.sh
@@ -8,7 +8,7 @@ EOF
 scratchOrgName=$(osascript -e "$applescriptCode");
 
 read -r -d '' applescriptCode1 <<'EOF'
-   set dialogText to text returned of (display dialog "Dev Hub Alias or UserName" default answer "girish@june2020uipath.com")
+   set dialogText to text returned of (display dialog "Dev Hub Alias or UserName" default answer "UIPATH_PROD")
    return dialogText
 EOF
 
@@ -21,11 +21,11 @@ sfdx force:org:create -f config/project-scratch-def.json -d 7 -s -a $scratchOrgN
 
 echo Installing uipath-core package
 coreLatestVersion=$(sfdx force:package:version:list -p 0Ho4K000000PB0iSAG -v $devHub -o CreatedDate --concise | tail -1 | awk '{print $3}')
-sfdx force:package:install --package $coreLatestVersion -u $scratchOrgName -w 10 -k test1234
+sfdx force:package:install --package $coreLatestVersion -u $scratchOrgName -w 10 -k unlockedclear
 
 echo Installing uipath-commons package
 commonsLatestVersion=$(sfdx force:package:version:list -p 0Ho4K000000PB0nSAG -v $devHub -o CreatedDate --concise | tail -1 | awk '{print $3}')
-sfdx force:package:install --package $commonsLatestVersion -u $scratchOrgName -w 15 -k test1234
+sfdx force:package:install --package $commonsLatestVersion -u $scratchOrgName -w 15 -k unlockedclear
 
 echo Installing uipath-commons package
 sfdx force:source:tracking:reset
